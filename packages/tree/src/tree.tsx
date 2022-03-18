4995694c618086706f098480e84f5f417ea1dba7
aoao <zhengjiaqi@illasoft.com>
2022-03-18 19:35:14 +0800

    temporary

--- packages/tree/src/tree.tsx
+++ packages/tree/src/tree.tsx
@@ -2,13 +2,38 @@
 import * as React from "react"
 import { forwardRef } from "react"
 import { TreeProps } from "./interface"
+import { List } from "@illa-design/list"
+import { ListItem, ListItemMeta } from "@illa-design/list/src"
+import { TreeNode } from "./tree-node"
+import { loopNode } from "./utils"
+
+export function getNodeList() {}
 
 export const Tree = forwardRef<HTMLDivElement, TreeProps>((props, ref) => {
-  const { ...rest } = props
+  const {
+    treeData = [],
+    onDragOver,
+    onDragLeave,
+    onDragStart,
+    onDrop,
+    onSelect,
+    ...rest
+  } = props
+
+  console.log("treeData", treeData)
+  const list = loopNode(treeData)
 
   return (
     <div ref={ref} {...rest}>
-      Tree
+      {list?.map((item) => {
+        return <TreeNode {...item} />
+      })}
+      {/*<List*/}
+      {/*  data={treeData}*/}
+      {/*  render={(data) => {*/}
+      {/*    return <TreeNode key={data.key} title={data.title} />*/}
+      {/*  }}*/}
+      {/*/>*/}
     </div>
   )
 })
